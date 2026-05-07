70ea204b276d168339d5cb8d1fbeb33e8423bb8a
hjd <hjd@atguigu.com>
2026-05-07 11:53:15 +0800

    gi commit -m "版本待升级"

---
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -0,0 +1,29 @@
+package cn.itcast.user.web;
+
+import cn.itcast.user.pojo.User;
+import cn.itcast.user.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 路径： /user/110
+     *
+     * @param id 用户id
+     * @return 用户
+     */
+    @GetMapping("/{id}")
+    public User queryById(@PathVariable("id") Long id) {
+        return userService.queryById(id);
+    }
+}
